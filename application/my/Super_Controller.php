6bfe37a14b51a92f14b97ca9bc29461036ecf9f4
Ali Kazim <ali.kazim@genetechsolutions.com>
2014-09-29 12:49:21 +0500

    Helpers and Core File Data 2

---
+++ application/my/Super_Controller.php
@@ -0,0 +1,5 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+require_once(APPPATH.'core/AdminController.php');
+
+require_once(APPPATH.'core/FrontendController.php');
